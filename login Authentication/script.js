b2f9be4866f13235f291b8473716cf594866e40b
Hemachandran R <94829963+hemachandran-r@users.noreply.github.com>
2023-09-24 10:05:34 +0530

    Add files via upload

---
+++ login Authentication/script.js
@@ -0,0 +1,33 @@
+document.getElementById("login-form").addEventListener("submit", async function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    // Simulate an API request (replace with actual API call)
+    const response = await mockApiLogin(username, password);
+
+    if (response.success) {
+        // Redirect to the welcome page
+        window.location.href = "welcome.html";
+    } else {
+        document.getElementById("error-message").textContent = response.message;
+    }
+});
+
+// Mock API function for login (replace with actual API call)
+async function mockApiLogin(username, password) {
+    // Simulate a delay for API response (remove in production)
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    if (username === "hemachandran" && password === "9626390730H,.") {
+        return { success: true };
+    } else {
+        return { success: false, message: "Invalid username or password" };
+    }
+}
+if (response.success) {
+    // Redirect to the welcome page
+    window.location.href = "welcome.html";
+}
+
